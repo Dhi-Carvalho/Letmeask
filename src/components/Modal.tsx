67b436a79137f0401690057d5ca97d5e8356255c
Dhi Carvalho <dhi.carvalho85@gmail.com>
2021-07-15 19:31:13 -0300

    Code refactoring

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,26 +1,14 @@
 import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { useHistory, useParams } from "react-router-dom";
-
-import danger from "../assets/images/danger.svg";
-import { database } from "../services/firebase";
 
 import "../styles/modal.scss";
-
-type RoomParams = {
-  id: string;
-};
 
 interface ModalProps {
   isShowing: boolean;
   toggle: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ isShowing, toggle }) => {
-  const history = useHistory();
-  const params = useParams<RoomParams>();
-  const roomId = params.id;
-
+const Modal: React.FC<ModalProps> = ({ isShowing, toggle, children }) => {
   useEffect(() => {
     const listener = function (e: KeyboardEvent) {
       if (e.key === "Escape" || e.key === "Esc" || e.keyCode === 27) {
@@ -38,37 +26,30 @@ const Modal: React.FC<ModalProps> = ({ isShowing, toggle }) => {
     };
   }, [isShowing, toggle]);
 
-  async function handleEndRoom() {
-    await database.ref(`rooms/${roomId}`).update({
-      endedAt: new Date(),
-    });
-
-    history.push("/");
-  }
-
   return isShowing
     ? ReactDOM.createPortal(
         <div className="modal-overlay">
           <div className="modal-wrapper">
-            <div className="modal">
-              <img src={danger} alt="Ação Perigosa" />
-              <h1>Encerrar sala</h1>
-              <p>Tem certeza que você deseja encerrar esta sala?</p>
-              <div className="buttons">
-                <button className="cancel" onClick={toggle}>
-                  Cancelar
-                </button>
-                <button className="delete" onClick={handleEndRoom}>
-                  Sim, encerrar
-                </button>
-              </div>
-            </div>
+            <div className="modal">{children}</div>
           </div>
         </div>,
         document.body
       )
     : null;
 };
+
+interface ModalFooterProps {
+  toggle: () => void;
+}
+
+export const ModalBody: React.FC = ({ children }) => (
+  <div className="modal-body">{children}</div>
+);
+
+export const ModalFooter: React.FC<ModalFooterProps> = ({
+  toggle,
+  children,
+}) => <div className="modal-footer">{children}</div>;
 
 export const useModal = () => {
   const [isShowing, setIsShowing] = useState(false);
